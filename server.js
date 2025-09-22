396f0a2da78b951c3875adf7770a210ffb52af52
arjit-web <tomararjit110@gmail.com>
2025-09-22 11:56:43 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+// server.js
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+// Fallback route (for single-page apps)
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname,"index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
