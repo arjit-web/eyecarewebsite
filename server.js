7e38e71c1d04a58a21959ae0dcdeec5433101c9d
VKS <krishna2421w@gmail.com>
2025-09-22 17:21:27 +0530

    api integration

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 
 
-app.use("/", express.static(path.join(__dirname, "images")));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use("/api", require('./modules/Appointment'));
 
